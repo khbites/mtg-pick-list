7a5d39285e568cae6742cf90f772ac7551c4a0bc
Leonard Tulipan <knowhowbites@gmail.com>
2013-10-24 13:48:42 +0100

    first fully working version. output is a challange to read though

--- comment_mtgo_draft.sh
+++ comment_mtgo_draft.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Author: Leonard Tulipan
 # Comment a MTGO Draft (recorded with Draft Recorder Option to USERNAME\Documents\Games\Magic The Gathering\Draft\*.txt)
+# dont forget to do dos2unix to get rid of CTRL-M at the line end
 
 draftlist=$1
 cardlist=output_formated_numeric.csv
@@ -10,7 +11,14 @@ i=1
 while [ $i -le $lines ]
  do
   line=`head -n $i $draftlist |tail -n 1`
-  name=`echo $line|sed 's/^\s\s\s\s//g' |sed 's/-->\s//g'` ;
-  echo $name
+  name=`echo $line|sed 's/^\s\s\s\s//g' |sed 's/-->\s//g' `
+  num=""
+#  echo "---"+ $name +"..."
+  echo $line
+  if [ "$name" != "" ]
+   then
+    cat $cardlist |awk -F ";" "/$name/ {print \$2; end}"
+  fi
+#  echo $line" "$num
   i=$[i+1]
 done
